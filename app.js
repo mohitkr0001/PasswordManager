7c62d1be272371fde4c3e103be043d1f006936a9
Mohit Kumar Singh <mohitkr0231@gmail.com>
2017-05-13 05:01:19 +0530

    Adding crypto-js to encrypt the user details

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 console.log("Started");
+let crypto=require('crypto-js');
 
 var storage=require('node-persist');
 storage.initSync();
@@ -24,6 +25,12 @@ let argv=require('yargs')
 			type:'string',
 			alias:'p',
 			description:'Put you secret password here'
+		},
+		masterpassword:{
+			demand:true,
+			type:'string',
+			alias:'m',
+			description:'Enter the master password'
 		}
 	}).help('help');
 })
@@ -34,29 +41,55 @@ let argv=require('yargs')
 			type:'string',
 			alias:'n',
 			description:'Enter social media name'
+		},
+		masterpassword:{
+			demand:true,
+			type:'string',
+			alias:'m',
+			description:'Enter the master password'
 		}
 	}).help('help');
 })
+.help('help')
 .argv;
 
 
 let command=argv._[0];
 
+function getAccounts(masterpassword){
 
-function createAccount(accounts){
-	var account=storage.getItemSync('account');
-	if(typeof account==='undefined')
-	{
-		account=[];
+	let encryptedData=storage.getItemSync('account');
+	let account=[];
+
+	if(typeof encryptedData !== 'undefined'){
+	let bytes=crypto.AES.decrypt(encryptedData,masterpassword);
+	account=JSON.parse(bytes.toString(crypto.enc.Utf8));
 	}
-	account.push(accounts);
-	storage.setItemSync('account',account);
+
+	return account;
+}
+
+
+
+function saveAccount(account,masterpassword){
+
+	let enaccount=crypto.AES.encrypt(JSON.stringify(account),masterpassword);	
+	storage.setItemSync('account',enaccount);
+
+	return account;
+}
+
+
+function createAccount(account,masterpassword){
+	let accounts=getAccounts(masterpassword);
+	accounts.push(account);
+	saveAccount(accounts,masterpassword);
 
 		return account;
 }
 
-function getAccount(accountName){
-	var account=storage.getItemSync('account');
+function getAccount(accountName,masterpassword){
+	let account=getAccounts(masterpassword);
 	var holdItem;
 	account.forEach(function(account){
 		if(account.name===accountName){
@@ -72,13 +105,13 @@ if(command==='create'){
 		name:argv.name,
 		username:argv.username,
 		password:argv.password
-	});
+	},argv.masterpassword);
 
 console.log('Account Created');
 console.log(store);
 }
 else if(command==='get'){
-	let show=getAccount(argv.name);
+	let show=getAccount(argv.name,agrv.masterpassword);
 
 	if(typeof show==='undefined'){
 		console.log('Sorry but your account is not found');
