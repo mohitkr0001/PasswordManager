597e0d16546582d705e3a88b96d0eb589d7b26bc
Mohit Kumar Singh <mohitkr0231@gmail.com>
2017-05-12 22:00:42 +0530

    Putting help option to show the decsription that help the user while entering the data

--- app_args.js
+++ app_args.js
@@ -1,7 +1,18 @@
-let argv=require('yargs').argv;
+let argv=require('yargs')
+.command('hello','Greet user',function(yargs){
+	yargs.options({
+		name:{
+			demand:true,
+			alias:'n',
+			description:'Your name goes here'
+		}
+	}).help('help');
+})
+.help('help')
+.argv;
 let arr=argv._[0];
 console.log(argv);
 
-if(typeof arr==='string' && typeof argv.name!=='undefined'&& argv.lastname!=='undefined'){
+if(typeof arr==='string' && typeof argv.name!=='undefined'&& typeof argv.lastname!=='undefined'){
 	console.log('Hey '+argv.name+' '+argv.lastname+'!');
 }
